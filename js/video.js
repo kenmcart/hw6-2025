60f58b89852003f7194e700cb201c952b545e433
kenmcart <kenmcart@umich.edu>
2025-04-03 01:38:45 -0400

    Yes

--- js/video.js
+++ js/video.js
@@ -2,10 +2,53 @@ var video;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
+	video = document.querySelector(".video");
+	video.autoplay = false;
+	video.loop = false;
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.9;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 1.1;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime = video.currentTime + 10;
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (!video.muted){
+		document.querySelector("#mute").innerHTML = "Unmute";
+		video.muted = true;
+	} else {
+		document.querySelector("#mute").innerHTML = "Mute";
+		video.muted = false;
+	}
+});
+
+document.querySelector("#slider").addEventListener('input', function() {
+	video.volume = document.querySelector("#slider").value / 100;
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
